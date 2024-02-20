89bcf352c0830cdce592a2ef5ced38b391255672
2536 <214471918@qq.com>
2024-02-20 17:46:15 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -1,10 +1,10 @@
 ### AnyKernel3 Ramdisk Mod Script
-## 酷安-心灯
+## @ 酷安-心灯
 
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=ExampleKernel by 酷安-心灯
+kernel.string=ExampleKernel by @ 酷安-心灯
 do.devicecheck=1
 do.modules=0
 do.systemless=1
